ebdb18169524e5aa1859a8a17cfea554bb1d7da1
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2023-03-03 23:51:17 +0600

    Add files via upload

---
+++ gauss elimination without pivot.m
@@ -0,0 +1,31 @@
+clc
+disp ("This program solves system of linear equations")
+disp ("provided the number of unknown variables & matrix")
+disp ("It is programmed without pivoting")
+
+n = input("Enter the number of variables : ");
+A = input("Enter the augmented matrix : ")
+
+   for i=1:n;
+        for j=i+1:n;
+                ratio=A(j,i)/A(i,i);
+                for k=i:n+1;
+                A(j,k)=A(j,k)-(ratio*A(i,k));
+              end
+              A
+        end
+    end
+
+
+        x(n)=A(n,n+1)/A(n,n);
+    for i=n-1:-1:1;
+        summ=0.;
+        for j=i+1:n;
+            summ=summ+A(i,j)*x(j);
+        end
+        x(i)=(A(i,n+1)-summ)/A(i,i);
+    end
+% [1 -1 1 1; -3 2 -3 -6;2 -5 4 5]
+% [27 6 -1 85;6 15 2 75; 1 1 54 110]
+% [2 6 -1 -12;5 -1 1 11; 4 -1 3 10]
+x

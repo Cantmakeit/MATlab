1ba5ec400d7377ab5ff680f06d765cd2b890ed20
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2023-03-04 11:36:14 +0600

    Add files via upload

---
+++ jordan_without_pivot.m
@@ -0,0 +1,30 @@
+clc
+clearvars
+disp ("This program solves system of linear equations")
+disp ("provided the number of unknown variables & matrix using gauss jordan")
+disp ("It is programmed without pivoting")
+
+n = input("Enter the number of variables : ");
+A = input("Enter the augmented matrix : ")
+
+   for i=1:n;
+        for j=1:n;
+                if i~=j
+                  ratio=A(j,i)/A(i,i);
+                for k=i:n+1;
+                A(j,k)=A(j,k)-(ratio*A(i,k));
+                  end
+              end
+              A
+        end
+    end
+
+    for i=n:-1:1;
+        A(i,n+1)= (A(i,n+1))/A(i,i);
+        A(i,i)=1;
+    end
+% [1 -1 1 1; -3 2 -3 -6;2 -5 4 5]
+% [27 6 -1 85;6 15 2 75; 1 1 54 110]
+% [2 6 -1 -12;5 -1 1 11; 4 -1 3 10]
+A
+A(:,n+1)'

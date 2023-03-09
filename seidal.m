4c7fe4af24896c6f6473dec3aebbaca1195d8be4
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2023-03-09 11:47:57 +0600

    Update seidal.m

--- seidal.m
+++ seidal.m
@@ -32,8 +32,9 @@ if flag==0
                     summ=summ+A(j,k)*x(k);
                 end
             end
+            temp=x(j);
 
-            temp=(A(j,n+1)-summ)/A(j,j);
+            x(j)=(A(j,n+1)-summ)/A(j,j);
             er=(abs(x(j)-temp)/temp);
 
             if er>tol;

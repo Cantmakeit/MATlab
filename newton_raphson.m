bec7da0999f3bd94522519b22d0dd873930b2bce
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2023-03-30 13:23:23 +0600

    Update newton_raphson.m

--- newton_raphson.m
+++ newton_raphson.m
@@ -7,10 +7,10 @@ x0=input('Enter the initial guess:');
 e=input('Enter the tolerance:');
 n=input('Enter the number of iterations:');
 for i=1:n
-    x1=x0 - vpa(subs(f,x,x0))/vpa(subs(g,x,x0))
+    x1=x0 - (subs(f,x,x0))/(subs(g,x,x0));
+     fprintf('x%d = %.4f\n',i,x1)
     if abs(x1-x0)<e
         break
     end    
     x0=x1;
 end    
-x1

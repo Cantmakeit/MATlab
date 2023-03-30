f6e28671f889f3ca8a12e360ed356d9844878085
Sanjida <64713109+Cantmakeit@users.noreply.github.com>
2023-03-30 13:22:45 +0600

    Update iterative.m

--- iterative.m
+++ iterative.m
@@ -1,14 +1,18 @@
-clc
-syms x
-g =  1/(9-x^2)
+close all
+clc 
+clear vars
+
+g=input('Enter the function:');
 x0=input('Enter the initial guess:');
 e=input('Enter the tolerance:');
 n=input('Enter the number of iterations:');
 for i=1:n
-    x1=vpa(subs(g,x,x0))
+    x1=g(x0);
+    fprintf('x%d = %.4f\n',i,x1)
     if abs(x1-x0)<e
         break
     end    
     x0=x1;
 end    
-x1
+%f=x^3+x^2-1;
+%@(x) (1+x)^(-0.5);
